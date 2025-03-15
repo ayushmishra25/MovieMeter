b325719bdf5837f7cb2e31186962684623026c00
Ayush Mishra <ayush.mishra@digivive.com>
2025-03-15 08:00:40 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { lazy, Suspense, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+import {
+  Header,
+  Footer,
+  SideBar,
+  VideoModal,
+  ScrollToTop,
+  Loader,
+} from "@/common";
+
+import "react-loading-skeleton/dist/skeleton.css";
+import "swiper/css";
+import { GA_MEASUREMENT_ID } from "./utils/config";
+
+const Catalog = lazy(() => import("./pages/Catalog"));
+const Home = lazy(() => import("./pages/Home"));
+const Detail = lazy(() => import("./pages/Detail"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(GA_MEASUREMENT_ID || "");
+    ReactGA.send("pageview");
+  }, []);
+
+  return (
+    <>
+      <VideoModal />
+      <SideBar />
+      <Header />
+      <main className=" lg:pb-14 md:pb-4 sm:pb-2 xs:pb-1 pb-0">
+        <ScrollToTop>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/:category/:id" element={<Detail />} />
+              <Route path="/:category" element={<Catalog />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </ScrollToTop>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
